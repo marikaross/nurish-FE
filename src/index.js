e636f04406a1c463d0e44b43278f5e766b2a383d
Marika Ross <marika.ross@gmail.com>
2018-09-10 14:07:53 -0600

    Add redux router and thunks to package json and setup index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import { BrowserRouter } from 'react-router-dom';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import thunk from 'redux-thunk';
+import './index.css';
 import registerServiceWorker from './registerServiceWorker';
+
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
+const router = (
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>
+);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
