1aca727318f8e063ca79908fe2737cf3314304c4
Marika Ross <marika.ross@gmail.com>
2018-09-10 14:36:51 -0600

    Finalize file structure with files

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import App from './containers/App/App';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import { BrowserRouter } from 'react-router-dom';
+import { rootReducer } from '../src/reducers';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
 import './index.css';
