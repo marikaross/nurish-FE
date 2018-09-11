521cad0975fb27d60683a3a2eaa2a89aef607281
Marika Ross <marika.ross@gmail.com>
2018-09-11 14:44:25 -0600

    Change action type to reflect the change in action creator name

--- src/reducers/formulaReducer.js
+++ src/reducers/formulaReducer.js
@@ -1,6 +1,8 @@
-export const formulaReducer = (state = [], action.type) => {
+import {addFormulas} from '../actions';
+
+export const formulaReducer = (state = [], action) => {
   switch(action.type) {
-    case ADD_FORMULA:
+    case 'ADD_FORMULAS':
     return [...state, ...action.formulas]
   default:
   return state
