9b1b1416acc130f02d769f1431be4907e89cda37
Marika Ross <marika.ross@gmail.com>
2018-09-11 14:42:38 -0600

    Add render method

--- src/components/FormulaContainer/FormulaContainer.js
+++ src/components/FormulaContainer/FormulaContainer.js
@@ -1,17 +1,40 @@
-import React from 'react'
+import React, { Component } from 'react';
 import { withRouter, NavLink } from 'react-router-dom';
+import {formulas} from '../../data-helper/mockFormula.js';
+import {FormulaCard} from '../FormulaCard/FormulaCard';
+import {connect} from 'react-redux'
 
 
+export class FormulaContainer extends Component{
 
-const FormulaContainer = () => {
-	return (
-		<div>
-			<NavLink to='/'>back</NavLink>
-			<NavLink to='/search'>search</NavLink>
-			<NavLink to='/calculate'>calculate</NavLink>
-			<p>I'm a formula container</p>
-		</div>
-	)
+
+  FormulaCards = () => {
+    console.log(formulas)
+
+    return formulas.map(formula => {
+      return (
+        <FormulaCard
+          key={formula.id}
+          {...formula}
+        />
+        )
+    })
+  }
+
+  render() {
+  	return (
+  		<div>
+  			<NavLink to='/'>back</NavLink>
+  			<NavLink to='/search'>search</NavLink>
+  			<NavLink to='/calculate'>calculate</NavLink>
+        {this.FormulaCards()}
+  		</div>
+  	)
+  }
 }
 
-export default withRouter(FormulaContainer);
+export const mapStateToProps = (state) => {
+  formulas: state.formulas
+}
+
+export default withRouter(connect(mapStateToProps, null)(FormulaContainer));
