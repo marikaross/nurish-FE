482a4dfeb359b3b050672afe2f77547b0c189ca6
Jeremiah Stanley <jeremiah.j.stanley@gmail.com>
2018-09-11 15:35:09 -0600

    Import FormulaContainer.css, remove calculate NavLink, and form-container and formula-container classes

--- src/components/FormulaContainer/FormulaContainer.js
+++ src/components/FormulaContainer/FormulaContainer.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import { withRouter, NavLink } from 'react-router-dom';
+import './FormulaContainer.css';
 
 
 
 const FormulaContainer = () => {
 	return (
-		<div>
+		<div className='form-container formula-container'>
 			<NavLink to='/'>back</NavLink>
 			<NavLink to='/search'>search</NavLink>
-			<NavLink to='/calculate'>calculate</NavLink>
 			<p>I'm a formula container</p>
 		</div>
 	)
