d79f4b9b3e79053c729a18658c6cbf629c078b0b
Jeremiah Stanley <jeremiah.j.stanley@gmail.com>
2018-09-12 08:20:42 -0600

    Add semantic ui Input, Select, and button; link stylesheet

--- src/containers/Search/Search.js
+++ src/containers/Search/Search.js
@@ -1,7 +1,14 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { withRouter, NavLink } from 'react-router-dom';
+import { Dropdown, Input, Button, Select } from 'semantic-ui-react';
+import './Search.css';
 
+const options = [
+  { key: 'name', text: 'name', value: 'name' },
+  { key: 'HPCP', text: 'HPCP', value: 'HPCP' },
+  { key: 'allergen', text: 'allergen', value: 'allergen' },
+]
 
 class Search extends Component {
 	constructor() {
@@ -14,17 +21,24 @@ class Search extends Component {
 
 	handleSubmit = (event) => {
 		event.preventDefault()
-		console.log('potato')
+		console.log(event)
 	}
 
 
 	render() {
 		return (
-			<div className='form-container'>
-				<NavLink to='/browse'>browse</NavLink>
+			<div className='form-container search-container'>
+				<div className='link-container'>
+					<NavLink to='/'>back</NavLink>
+					<NavLink to='/filter'>filter</NavLink>
+					<NavLink to='/browse'>browse</NavLink>
+				</div>
 				<form className='search-field' onSubmit={this.handleSubmit}>
-					<input placeholder='enter a search criteria'/>
-					<button>Search</button>
+				  <Input type='text' placeholder='enter your criteria' action>
+				    <input />
+				    <Select compact options={options} defaultValue='name' />
+				    <Button type='submit'>Search</Button>
+				  </Input>
 				</form>
 			</div>
 		)
