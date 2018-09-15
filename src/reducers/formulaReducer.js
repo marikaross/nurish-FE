fa8d4cc1072c4cf206b34c047a45923111309eaa
Marika Ross <marika.ross@gmail.com>
2018-09-15 11:45:17 -0600

    Add isLoading and hasErrored reducers

--- src/reducers/formulaReducer.js
+++ src/reducers/formulaReducer.js
@@ -10,3 +10,21 @@ export const formulaReducer = (state = [], action) => {
     return state
   }
 }
+
+export const isLoading = (state = false, action) => {
+  switch (action.type) {
+    case 'IS_LOADING':
+      return action.isLoading
+    default:
+      return state
+  }
+}
+
+export const hasErrored = (state = false, action) => {
+  switch (action.type) {
+    case 'HAS_ERRORED':
+      return action.hasErrored
+    default:
+      return state
+  }
+}
