7f0690eb82406f8719b3ef758186f83e53a7438d
Marika Ross <marika.ross@gmail.com>
2018-09-15 11:36:36 -0600

    Add isLoading and hasErrored for thunk use

--- src/actions/index.js
+++ src/actions/index.js
@@ -7,3 +7,13 @@ export const formulaFetchDataSuccess = (formulas) => {
   type: 'FORMULA_FETCH_DATA_SUCCESS',
   formulas
 }
+
+export const isLoading = (bool) => ({
+  type: 'IS_LOADING',
+  isLoading: bool
+})
+
+export const hasErrored = (bool) => ({
+  type: 'HAS_ERRORED',
+  hasErrored: bool
+})
