a8a592aa15ac194351932ea6e53d32e9620de785
Marika Ross <marika.ross@gmail.com>
2018-09-15 11:16:42 -0600

    Create formulaFetchDataSuccess action for thunk

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,3 +2,8 @@ export const addFormulas = (formulas) => {
   type: 'ADD_FORMULAS',
   formulas
 }
+
+export const formulaFetchDataSuccess = (formulas) => {
+  type: 'FORMULA_FETCH_DATA_SUCCESS',
+  formulas
+}
