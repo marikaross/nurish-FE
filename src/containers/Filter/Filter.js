53753d5074dd9b20b4c2292d8b71d42a756e5fe1
Jeremiah Stanley <jeremiah.j.stanley@gmail.com>
2018-09-16 17:00:17 -0600

    Refactor component, remove redundant filter methods, add new onChange methods

--- src/containers/Filter/Filter.js
+++ src/containers/Filter/Filter.js
@@ -4,9 +4,11 @@ import { withRouter, NavLink } from 'react-router-dom';
 import { Dropdown, Input, Button } from 'semantic-ui-react';
 import './Filter.css'
 
-const specialties = [{ key: 'VHP', value: 'High Protein', text: 'Very High Protein (VHP)'}]
+const specialties = [{ key: 'VHP', value: 'HighProtein', text: 'Very High Protein (VHP)'}]
 const allergies = [{ key: 'Gluten', value: 'Wheat', text: 'Wheat and/or Gluten'},
 { key: 'Corn', value: 'Corn', text: 'Corn'}, { key: 'Soy', value: 'Soy', text: 'Soy'}]
+const mctSplit = [{ key: '50:10:40', value: '50:10:40', text: '50:10:40'},
+{ key: '30:40:30', value: '30:40:30', text: '30:40:30'}, { key: '20:20:10', value: '20:20:10', text: '20:20:10'}]
 
 class Filter extends Component {
 	constructor() {
@@ -16,55 +18,44 @@ class Filter extends Component {
 			allergens: [],
 			specialty: [],
 			mct: []
-
 		}
 	}
 
 	handleSubmit = async (event) => {
 		event.preventDefault()
-		const allergens = await this.filterAllergens()
-		const specialty = await this.filterSpecialty()
-		const mct = await this.filterMCT()
-		this.compareFilter(allergens, specialty, mct)
+		const specialty = await this.fetchFormulas('specialty')
+		const allergens = await this.fetchFormulas('allergens')
+		const mct = await this.fetchFormulas('mct')
+		// this.filterFormulas(allergens, specialty, mct)
 	}
 
-	handleChange = (event, data) => {
+	handleSpecialty = (event, data) => {
+		this.setState({ specialty: [data.value]})
+	}
 
-		event.preventDefault()
+	handleAllergens = (event, data) => {
 		this.setState({ allergens: [...data.value]})
-		console.log(this.state.allergens)
 	}
 
-	compareFilter = (allergens = [], specialty = [], mct = []) => {
-		switch (allergens) {
-			case allergens.length:
-
-		}
- }
-
-	filterAllergens = async () => {
-		let urlParam = this.state.allergens.join()	
-		let url = `https://nurish-app.herokuapp.com/api/v1/formulas?allergens=${urlParam}`
+	handleMCT = (event, data) => {
+	this.setState({ mct: [data.value]})
+	}
+ 
+	fetchFormulas = async (criteria) => {
+		let urlParam = this.state[criteria].join()	
+		let url = `https://nurish-app.herokuapp.com/api/v1/formulas?${criteria}=${urlParam}`
 		const response = await fetch(url)
 		return await response.json()
 	}
 
-	filterSpecialty = async () => {
-		let urlParam = this.state.specialty.join()	
-		let url = `https://nurish-app.herokuapp.com/api/v1/formulas?specialty=${urlParam}`
-		const response = await fetch(url)
-		return await response.json()
-	}
+	// filterFormulas = (allergens = [], specialty = [], mct = []) => {
+	// 	switch (allergens) {
+	// 		case allergens.length:
 
-	filterMCT = async () => {
-		let urlParam = this.state.mct.join()
-		let url = `https://nurish-app.herokuapp.com/api/v1/formulas?specialty=${urlParam}`
-		const response = await fetch(url)
-		return await response.json()
-	}
+	// 	}
+ // }
 
 	render() {
-		const { value } = this.state
 		return (
 			<div className='form-container filter-container'>
 				<div className='link-container'>
@@ -73,14 +64,9 @@ class Filter extends Component {
 					<NavLink to='/browse'>browse</NavLink>
 				</div>
 				<form className='filter-form' onSubmit={this.handleSubmit}>
-				<Dropdown onChange={this.handleChange} placeholder='specialty' fluid search selection options={specialties} name="specialty" />
-				<Dropdown onChange={this.handleChange} placeholder='allergies' fluid multiple search selection options={allergies}/>
-				<Input list='mct splits' placeholder='mct split' />
-    			<datalist id='mct splits'>
-      			<option value='50:10:40' />
-     				<option value='30:40:30' />
-      			<option value='20:20:10' />
-    			</datalist>
+				<Dropdown onChange={this.handleSpecialty} placeholder='specialty' fluid search selection options={specialties}/>
+				<Dropdown onChange={this.handleAllergens} placeholder='allergies' fluid multiple search selection options={allergies}/>
+				<Dropdown onChange={this.handleMCT} placeholder='mct split' fluid search selection options={mctSplit}/>
     		<Button fluid>Search</Button>
 				</form>
 			</div>
