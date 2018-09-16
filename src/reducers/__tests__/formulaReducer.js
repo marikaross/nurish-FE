d1d7978a849747cb0b4daed1756d71195b2407e6
Marika Ross <marika.ross@gmail.com>
2018-09-16 07:01:05 -0600

    Fix linting errors

--- src/reducers/__tests__/formulaReducer.js
+++ src/reducers/__tests__/formulaReducer.js
@@ -4,39 +4,40 @@ import * as action from '../../actions';
 describe('formulaReducer', () => {
 
   it('should return the initial state when the action is undefined', () => {
-    const expected = []
-    const result = formulaReducer([], {})
+    const expected = [];
+    const result = formulaReducer([], {});
     expect(result).toEqual(expected);
-  })
+  });
 
   it('should add formulas to the state when the type is ADD_FORMULAS', () => {
-    const initialState = []
-    const formulas = [{title: 'Beneprotein'}]
-    const expected = [...formulas]
-    const result = formulaReducer(initialState, action.addFormulas(formulas))
-    expect(result).toEqual(expected)
-  })
+    const initialState = [];
+    const formulas = [{title: 'Beneprotein'}];
+    const expected = [...formulas];
+    const result = formulaReducer(initialState, action.addFormulas(formulas));
+    expect(result).toEqual(expected);
+  });
 
   it('should return the formulas state when the type is FORMULA_FETCH_DATA_SUCCESS', async () => {
-    const initialState = []
-    const formulas = [{name: 'Boost Pudding'}]
-    const expected = [...formulas]
-    const result = await formulaReducer(initialState, action.formulaFetchDataSuccess(formulas))
-    expect(result.toEqual(expected))
-  })
-})
+    const initialState = [];
+    const formulas = [{name: 'Boost Pudding'}];
+    const expected = [...formulas];
+    const result = await formulaReducer(initialState, action.formulaFetchDataSuccess(formulas));
+    expect(result.toEqual(expected));
+  });
+});
 
 describe('isLoading', () => {
   it('should return a boolean', () => {
-    const expected = false
-    const result = isLoading(false, action.isLoading)
-    expect(result).toEqual(expected)
-  })
-})
+    const expected = false;
+    const result = isLoading(false, action.isLoading);
+    expect(result).toEqual(expected);
+  });
+});
 
 describe('hasErrored', () => {
   it('should return a boolean', () => {
-    const expected = false
-    const result = hasErrored(false, action.hasErrored)
-  })
-})
+    const expected = false;
+    const result = hasErrored(false, action.hasErrored);
+    expect(result).toEqual(expected);
+  });
+});
