a4abf8bd4f139557c17c9c07c31ec63e5c10eece
Marika Ross <marika.ross@gmail.com>
2018-09-17 17:21:28 -0600

    Add filterReducer to root reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
-import { formulaReducer, isLoading, hasErrored } from './formulaReducer'
+import { formulaReducer, isLoading, hasErrored } from './formulaReducer';
+import { filterReducer } from './filterReducer';
 
 export const rootReducer = combineReducers({
   formulas: formulaReducer,
+  filterResults: filterReducer,
   isLoading,
   hasErrored
 })
