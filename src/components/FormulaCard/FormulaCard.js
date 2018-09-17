2cedbeb6a2745d8d5cc6d80419ba425af4a9b9c6
Jeremiah Stanley <jeremiah.j.stanley@gmail.com>
2018-09-17 14:54:00 -0600

    Change key property to be more unique on formulaCards

--- src/components/FormulaCard/FormulaCard.js
+++ src/components/FormulaCard/FormulaCard.js
@@ -3,21 +3,20 @@ import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import './FormulaCard.css';
 
-
 export const FormulaCard = (formula) => {
 
   const usageGuidelines = () => {
     let usageArray = formula.usage.split(',')
     return usageArray.map(usage => {
       return (
-        <li key={formula.id}>{usage}</li>
+        <li key={`formula-card-${formula.id}-/${usage}`}>{usage}</li>
       );
     });
   };
 
   return (
     <Link className="formula-link" to={`/browse/${formula.id}`}>
-      <div className='formula-card' key={formula.id}>
+      <div className='formula-card' key={`formula-card-${formula.id}`}>
         <h2>{formula.name}</h2>
         <h4>{formula.description}</h4>
         <ul>usage: {usageGuidelines()}</ul>
