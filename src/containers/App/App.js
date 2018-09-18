8a827dcb769d0df4f57ca040993ff8aa174683ad
Jeremiah Stanley <jeremiah.j.stanley@gmail.com>
2018-09-17 22:43:48 -0600

    Add alt tag on logo image, conditionally render Load component

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -5,6 +5,7 @@ import PropTypes from 'prop-types';
 import FormulaContainer from '../FormulaContainer/FormulaContainer';
 import DetailsCard from '../../components/DetailsCard/DetailsCard';
 import { fetchFormula } from '../../thunks/fetchFormula.js';
+import Load from '../../components/Loader/Loader';
 import { addFormulas } from '../../actions';
 import Calculate from '../Calculate/Calculate';
 import Filter from '../Filter/Filter';
@@ -43,14 +44,15 @@ class App extends Component {
     const classes = `nurish-logo ${this.state.position}`
     return (
       <div className='app'>
-        <img className={classes} src='/images/nurish-logo.gif'/>
+        <img className={classes} alt='nurish-logo' src='/images/nurish-logo.gif'/>
+        { this.props.isLoading ?  <Load/> : ''}
         <Route exact path='/' component={Form}/>
         <Route exact path='/filter' component={Filter}/>
 				<Route exact path='/search' component={Search}/>
 				<Route exact path='/calculate' component={Calculate}/>
 				<Route exact path='/formulas' render={() => <FormulaContainer animateLogo={this.animateLogo} />}/>
         <Route exact path='/formulas/:id' render={({ match }) => {
-          const formula = this.props.formulas.find(formula => formula.id == match.params.id);
+          const formula = this.props.formulas.find(formula => formula.id === parseInt(match.params.id, 10));
           return (
             <div>
               <DetailsCard formula={formula} animateLogo={this.animateLogo}/>
