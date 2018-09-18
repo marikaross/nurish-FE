17bbf07e5bad02d8308e45025e5d4c4a22168c1b
Jeremiah Stanley <jeremiah.j.stanley@gmail.com>
2018-09-17 22:33:47 -0600

    Remove unnecessary import and lint file

--- src/reducers/filterReducer.js
+++ src/reducers/filterReducer.js
@@ -1,12 +1,11 @@
-import {addFilterResults, filterResultFetchDataSuccess} from '../actions';
-
 export const filterReducer = (state = [], action) => {
   switch(action.type) {
     case "ADD_FILTER_RESULTS":
-      return [...state, ...action.filterResults]
+      return [...state, ...action.filterResults];
     case 'FILTER_RESULT_FETCH_DATA_SUCCESS':
-      return action.filterResults
-    default: return state
+      return action.filterResults;
+    default: 
+    	return state;
   }
 }
 
