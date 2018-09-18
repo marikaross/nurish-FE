d0851b625f5327ba2afdea82ce35b3da4f67db2c
Marika Ross <marika.ross@gmail.com>
2018-09-18 15:59:01 -0600

    Add MSTP test and update snapshot

--- src/containers/FormulaContainer/__tests__/FormulaContainer.js
+++ src/containers/FormulaContainer/__tests__/FormulaContainer.js
@@ -1,22 +1,47 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import FormulaContainer from '../FormulaContainer';
+import {FormulaContainer, mapStateToProps, mapDispatchToProps} from '../FormulaContainer';
+import * as action from '../../../actions';
 
 describe('FormulaContainer', () => {
   let wrapper
   let mockFilterResultCard
   let mockFormulaCard
+  let mockAddFilterResults
+  let mockFilterResults
+  let mockAnimateLogo
 
   beforeEach(() => {
     mockFormulaCard = jest.fn();
     mockFilterResultCard = jest.fn();
+    mockAddFilterResults = jest.fn();
+    mockFilterResults = [{title: 'boost'}, { title: 'smoothie'}];
+    mockAnimateLogo = jest.fn()
+
     wrapper = shallow(<FormulaContainer
       formulaCard={mockFormulaCard}
       filterResultCard={mockFilterResultCard}
-      />)
-  })
+      filterResults={mockFilterResults}
+      mockAddFilterResults={mockAddFilterResults}
+      animateLogo={mockAnimateLogo}
+      />);
+  });
 
   it('should match the snapshot', () => {
     expect(wrapper).toMatchSnapshot()
   })
-})
+
+    it('should return a filterResults array', () => {
+    const mockState = {
+      filterResults: mockFilterResults,
+      addFilterResults: mockAddFilterResults
+    };
+
+    const expectedProps = {
+      filterResults: mockFilterResults
+    };
+
+    const mappedProps = mapStateToProps(mockState);
+    expect(mappedProps).toEqual(expectedProps);
+  });
+});
