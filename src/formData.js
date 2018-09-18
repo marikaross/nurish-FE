3ddf420af4208a4c0a33601b9123e27f0a3f01a6
Jeremiah Stanley <jeremiah.j.stanley@gmail.com>
2018-09-17 22:34:11 -0600

    Remove searchCriteria variable and lint file

--- src/formData.js
+++ src/formData.js
@@ -1,10 +1,3 @@
-
-export const searchCriteria = [
-  { key: 'name', text: 'name', value: 'name' },
-  { key: 'hcpc', text: 'hcpc', value: 'hcpc' },
-  { key: 'allergen', text: 'allergen', value: 'allergen' },
-]
-
 export const specialties = [
 	{ key: 'vhp', value: 'vhp', text: 'very high protein (vhp)'},
 	{ key: 'vhc', value: 'vhc', text: 'very high calorie (vhc)'},
@@ -12,16 +5,16 @@ export const specialties = [
 	{ key: 'renal', value: 'renal', text: 'renal'},
 	{ key: 'semi-elemental', value: 'semi-elemental', text: 'gut malabsorption (semi-elemental)'},
 	{ key: 'pediatric', value: 'pediatric', text: 'pediatric)'},
-]
+];
 
 export const allergies = [
 	{ key: 'Gluten', value: 'Wheat', text: 'Wheat and/or Gluten'},
 	{ key: 'Corn', value: 'Corn', text: 'Corn'}, 
 	{ key: 'Soy', value: 'Soy', text: 'Soy'}
-]
+];
 
 export const mctSplit = [
 	{ key: 'low', value: 'low', text: 'low'},
 	{ key: 'medium', value: 'medium', text: 'medium'}, 
 	{ key: 'high', value: 'high', text: 'high'}
-]
+];
