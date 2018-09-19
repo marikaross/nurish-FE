20b5721f9b9571c28cb0e334d91866a7ffc5380c
Jeremiah Stanley <jeremiah.j.stanley@gmail.com>
2018-09-18 23:42:21 -0600

    Unskip history routing test to see if failure persists in ci build

--- src/containers/Search/__tests__/Search.js
+++ src/containers/Search/__tests__/Search.js
@@ -75,7 +75,7 @@ describe('Search', () => {
     expect(mockFetchResults).toHaveBeenCalledWith(url);
   });
 
-   it.skip('should direct the user to the formulas page of the application', () => {
+   it('should direct the user to the formulas page of the application', () => {
     wrapper.instance().handleInput(mockInput);
 
     wrapper.instance().handleSubmit(mockEvent);
