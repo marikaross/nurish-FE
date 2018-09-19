78bd39350d58db2b21c91859db0de5c8010fdb1c
Jeremiah Stanley <jeremiah.j.stanley@gmail.com>
2018-09-18 19:05:24 -0600

    Dynamically render nutritional content and usage, remove mock nutritional content variable

--- src/components/DetailsCard/DetailsCard.js
+++ src/components/DetailsCard/DetailsCard.js
@@ -1,50 +1,33 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import Load from '../Loader/Loader';
 import { withRouter, NavLink } from 'react-router-dom';
+import { fetchSingleFormula } from '../../thunks/fetchSingleFormula.js';
+import PropTypes from 'prop-types';
 import './DetailsCard.css';
 
-let features ={ 
-	"Caloric Density": "1.0 Cal/ml",
-	"Protein": "17%",
-	"Carbohydrate": "Many",
-	"Fat": "so much"
-};
-
-let nutritionalContent= {
-  "Calories": {
-    "7 g*": 25,
-    "% Daily Value": ""
-  },
-  "Fat": {
-    "7 g*": "0 g",
-    "% Daily Value": "0%"
-  },
-  "Sodium": {
-    "7 g*": "15 mg",
-    "% Daily Value": "1%"
-  },
-  "Potassium": {
-    "7 g*": "35 mg",
-    "% Daily Value": "2%"
-  },
-  "Carbohydrate": {
-    "7 g*": "0 g",
-    "% Daily Value": "0%"
-  },
-  "Protein": {
-    "7 g*": "6 g",
-    "% Daily Value": ""
-  },
-  "Calcium": {
-    "7 g*": "",
-    "% Daily Value": "2%"
-  },
-  "Limitations": "Not a significant source of saturated fat, trans fat, cholesterol, fibre, sugars, vitamin A, vitamin C, or iron.",
-  "Serving size": "* 1 scoop or packet"
-};
-
 class DetailsCard extends Component {
+  constructor() {
+    super()
 
-  componentDidMount() {
+    this.state = {
+
+    }
+  }
+
+  static getDerivedStateFromProps(props, state) {
+    if (props.formula) {
+      return {
+        formula: props.formula
+      };
+    }
+    return state 
+  }
+  
+  async componentDidMount() {
+    let id = new URL(window.location.href);
+    let url = `https://nurish-app.herokuapp.com/api/v1${id.pathname}`
+    await this.props.fetchSingleFormula(url);
     this.props.animateLogo('collapse-logo')
   }
 
@@ -53,7 +36,7 @@ class DetailsCard extends Component {
   }
 	
 	usageGuidelines = () => {
-    return this.props.formula.usage.split(',').map(use => {
+    return this.state.formula.usage.split(',').map(use => {
       return (
           <li>{use}</li>
         );
@@ -61,17 +44,21 @@ class DetailsCard extends Component {
   };
 
   featuresTable = () => {
-		return Object.keys(features).map(feature => {
-			return (
-				<li>{feature}: {features[feature]}</li>
-			);
+		return Object.keys(this.state.formula.formula_overview).map(feature => {
+      if (typeof this.state.formula.formula_overview[feature] === 'string') {
+        let regex = /_/gi;
+        let featureName = feature.replace(regex, ' ');
+  			return (
+  				<li>{featureName}: {this.state.formula.formula_overview[feature]}</li>
+  			);
+      }
 		});
   };
 
   nutritionalHeaders = () => {
-  	let nutrients = Object.keys(nutritionalContent);
-  	let headers = Object.keys(nutritionalContent[nutrients[0]])
-  	return headers.map((nutrientHeader, index) => {
+  	let nutrients = Object.keys(this.state.formula.nutritional_content.properties);
+  	let headers = Object.keys(this.state.formula.nutritional_content.properties[nutrients[0]])
+  	return headers.map((nutrientHeader, index, arr) => {
       let className = `nutrient-header-${index + 1}`
   		return (
   			<span className={className}>{nutrientHeader}</span>
@@ -80,22 +67,22 @@ class DetailsCard extends Component {
   };
 
   nutritionalTable = () => {
-  	return Object.keys(nutritionalContent).map(nutrient => {
-  		if (typeof nutritionalContent[nutrient] === 'object') {
-	  		var content = Object.keys(nutritionalContent[nutrient]).map(unit => {
+    return Object.keys(this.state.formula.nutritional_content.properties).map(nutrient => {
+  		if (typeof this.state.formula.nutritional_content.properties[nutrient] === 'object') {
+	  		var content = Object.keys(this.state.formula.nutritional_content.properties[nutrient]).map(unit => {
 	  			return (
 	  				<span>
-	  					{nutritionalContent[nutrient][unit]}
+	  					{this.state.formula.nutritional_content.properties[nutrient][unit]}
 	  				</span>
 	  			);
 	  		});
   		} else {
-  			return <span className='serving-size'>{nutrient}: {nutritionalContent[nutrient]}</span>	
+  			return <span className='serving-size'>{nutrient}: {this.state.formula.nutritional_content.properties[nutrient]}</span>	
   		};
 
 		return (
 			<li className='nutrient-row'>
-  			<span className>
+  			<span>
   				{nutrient}:
   			</span>
   				{content}
@@ -105,49 +92,69 @@ class DetailsCard extends Component {
   };
 
   render() {
-    return (
-      <div className='details-card'>
-        <div className='link-container'>
-          <a onClick={this.props.history.goBack}>back</a>
-          <NavLink to='/search'>search</NavLink>
-          <NavLink to='/filter'>filter</NavLink>
-          <NavLink to='/formulas'>browse</NavLink>
+    if (this.state.formula.usage) {
+      let url = `https://raw.githubusercontent.com/shebesabrina/Nurish-BE/images/public/formula%20images/${this.props.formula.image}`
+      return (
+        <div className='details-card'>
+          <div className='link-container'>
+            <a onClick={this.props.history.goBack}>back</a>
+            <NavLink to='/search'>search</NavLink>
+            <NavLink to='/filter'>filter</NavLink>
+            <NavLink to='/formulas'>browse</NavLink>
+          </div>
+          <div className='single-card'>
+          	<h1 className='formula-title'>{this.props.formula.title}</h1>
+            <div className='formula-image' alt=''style={ { backgroundImage: `url(${url})` } }>
+            </div>
+          	<p className='single-formula-description'>
+          		{this.props.formula.description}
+          	</p>
+            <p className='single-formula-restrictions'>
+              <strong>{this.props.formula.restrictions}</strong>
+            </p>
+          	<h2 className='nutritional-management-header'>
+          		for the nutritional management of:
+          	</h2>
+          	<ul className='usage-guidelines'>
+          		{this.usageGuidelines()}
+      			</ul>
+      			<h2 className='features-header'>
+      				features at a glance:
+      			</h2>
+      			<ul className='features-table'>
+      				{this.featuresTable()}
+      			</ul>
+      			<h2 className='nutritional-information-header'>
+      				nutritional information:
+      			</h2>
+      			<ul className='nutritional-information'>
+      				 <span className='nutrient-headers'>{this.nutritionalHeaders()}</span>
+      				{this.nutritionalTable()}
+      			</ul>
+      			<p className='ingredients-text'><span className='ingredients-title'><strong>ingredients: </strong></span>
+      				{this.props.formula.ingredients}
+      			</p>
+          </div>
         </div>
-        <div className='single-card'>
-        	<h1 className='formula-title'>{this.props.formula.title}</h1>
-        	<img className='formula-image' alt='' src='https://github.com/jeremiahjstanley/pattrn-party/blob/master/css-resources/placeholder.png?raw=true'/>
-        	<p className='single-formula-description'>
-        		{this.props.formula.description}
-        	</p>
-          <p className='single-formula-restrictions'>
-            <strong>{this.props.formula.restrictions}</strong>
-          </p>
-        	<h2 className='nutritional-management-header'>
-        		for the nutritional management of:
-        	</h2>
-        	<ul className='usage-guidelines'>
-        		{this.usageGuidelines()}
-    			</ul>
-    			<h2 className='features-header'>
-    				features at a glance:
-    			</h2>
-    			<ul className='features-table'>
-    				{this.featuresTable()}
-    			</ul>
-    			<h2 className='nutritional-information-header'>
-    				nutritional information:
-    			</h2>
-    			<ul className='nutritional-information'>
-    				 <span className='nutrient-headers'>{this.nutritionalHeaders()}</span>
-    				{this.nutritionalTable()}
-    			</ul>
-    			<p className='ingredients-text'><span className='ingredients-title'><strong>ingredients: </strong></span>
-    				{this.props.formula.ingredients}
-    			</p>
-        </div>
-      </div>
-    ); 
+      ); 
+    } else 
+      return (
+        <Load/>
+      )
   }
 };
 
-export default withRouter(DetailsCard);
+export const mapStateToProps = (state) => ({
+  formula: state.singleFormula
+});
+
+export const mapDispatchToProps = (dispatch) => ({
+  fetchSingleFormula: (url) => dispatch(fetchSingleFormula(url))
+});
+
+DetailsCard.propTypes = {
+  fetchSingleFormula: PropTypes.func,
+  formula: PropTypes.object
+};
+
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(DetailsCard));
