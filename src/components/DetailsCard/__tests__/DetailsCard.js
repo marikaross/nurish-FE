a2ba57819c89a596c467fe47ad6083aa25596a71
Marika Ross <marika.ross@gmail.com>
2018-09-18 23:26:29 -0600

    New snapshot for conditional rendering

--- src/components/DetailsCard/__tests__/DetailsCard.js
+++ src/components/DetailsCard/__tests__/DetailsCard.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { shallow } from 'enzyme';
+import { Load } from '../../Loader/Loader';
 import {DetailsCard, mapStateToProps, mapDispatchToProps} from '../DetailsCard.js';
 import { fetchSingleFormula } from '../../../thunks/fetchSingleFormula.js';
 import * as action from '../../../actions';
@@ -19,13 +20,19 @@ describe('DetailsCard', () => {
     mockhistory = []
     mockAnimateLogo = jest.fn()
     mockFetchSingleFormula = jest.fn()
-    mockformula = {nutritional_content: {properties:{nutrient: 'sodium'}, formula_overview: 'best ever', usage: 'thirst'}}
+    mockformula = {nutritional_content: {properties:{nutrient: 'sodium'}, formula_overview: 'best ever', usage: 'thirst', image: 'nowhere'}}
     wrapper = shallow(<DetailsCard fetchSingleFormula={mockFetchSingleFormula} formula={mockformula} history={mockhistory} animateLogo={mockAnimateLogo} />)
   });
 
   it('should match the snapshot', () => {
     expect(wrapper).toMatchSnapshot();
   });
+
+  it('should render the appropriate content if there is info in the state', () => {
+    const loadWrapper = shallow(<Load />)
+    wrapper.setState()
+    expect(wrapper).toMatchSnapshot()
+  })
 
   it('should return a formula object', () => {
       const mockState = {
