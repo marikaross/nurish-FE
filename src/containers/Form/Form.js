7ed8905b6d35c8c6973e33d4fd03c3512be61cd4
Sabrina Robinson <sabrinarob@gmail.com>
2018-12-15 11:51:35 -0700

    Wraps formatted message in nav links

--- src/containers/Form/Form.js
+++ src/containers/Form/Form.js
@@ -1,14 +1,17 @@
 import React, { Component } from 'react';
 import { NavLink, withRouter } from 'react-router-dom';
-import './Form.css'; 
+import { FormattedMessage } from 'react-intl';
+import './Form.css';
 
 export class Form extends Component {
   render() {
     return (
       <div className="form-container links">
-				<NavLink to='/search'>search</NavLink>
-				<NavLink to='/filter'>filter</NavLink>
-				<NavLink to='/formulas'>browse</NavLink>
+  				<NavLink to='/search'><FormattedMessage className="form-container links" id="Form.search-button" defaultMessage="Search"/></NavLink>
+
+  				<NavLink to='/filter'><FormattedMessage className="form-container links" id="Form.filter-button" defaultMessage="Filter"/></NavLink>
+
+  				<NavLink to='/formulas'><FormattedMessage className="form-container links" id="Form.browse-button" defaultMessage="Browse"/></NavLink>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './containers/App/App';
 import { Provider } from 'react-redux';
+import { IntlProvider } from 'react-intl';
 import { createStore, applyMiddleware } from 'redux';
 import { BrowserRouter } from 'react-router-dom';
 import { rootReducer } from '../src/reducers';
@@ -11,11 +12,13 @@ import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 
 const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
-
+// let { lang } = store.getState().locales;
 ReactDOM.render(
   <Provider store={store}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <IntlProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+      </IntlProvider>
   </Provider>, document.getElementById('root'));
 registerServiceWorker();
