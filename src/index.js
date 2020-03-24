9a56e6cb562c16bec45e4b6c708e2532bcde9777
Jeremiah Stanley <jeremiah.j.stanley@gmail.com>
2020-03-24 10:44:31 -0600

    Lint file

--- src/index.js
+++ src/index.js
@@ -1,21 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './containers/App/App';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
-import { BrowserRouter } from 'react-router-dom';
-import { rootReducer } from '../src/reducers';
-import { composeWithDevTools } from 'redux-devtools-extension';
-import thunk from 'redux-thunk';
-import './index.css';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./containers/App/App";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import { BrowserRouter } from "react-router-dom";
+import { rootReducer } from "../src/reducers";
+import { composeWithDevTools } from "redux-devtools-extension";
+import thunk from "redux-thunk";
+import "./index.css";
+import registerServiceWorker from "./registerServiceWorker";
 
-const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(thunk))
+);
 
 ReactDOM.render(
   <Provider store={store}>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </Provider>, document.getElementById('root'));
+  </Provider>,
+  document.getElementById("root")
+);
 registerServiceWorker();
