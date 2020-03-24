3868e483f87b4bdc930a9a6446d13f2823f28d51
Jeremiah Stanley <jeremiah.j.stanley@gmail.com>
2020-03-24 10:48:33 -0600

    Lint file

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,11 @@
-import { combineReducers } from 'redux';
-import { formulaReducer, singleFormulaReducer, isLoading, hasErrored } from './formulaReducer';
-import { filterReducer } from './filterReducer';
+import { combineReducers } from "redux";
+import {
+  formulaReducer,
+  singleFormulaReducer,
+  isLoading,
+  hasErrored
+} from "./formulaReducer";
+import { filterReducer } from "./filterReducer";
 
 export const rootReducer = combineReducers({
   formulas: formulaReducer,
