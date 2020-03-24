2fa74667393c61c3710013b50beed66d23393438
Jeremiah Stanley <jeremiah.j.stanley@gmail.com>
2020-03-24 10:46:55 -0600

    Lint file

--- src/containers/FormulaContainer/FormulaContainer.js
+++ src/containers/FormulaContainer/FormulaContainer.js
@@ -1,60 +1,50 @@
-import React, { Component } from 'react';
-import { withRouter, NavLink } from 'react-router-dom';
-import { FormulaCard } from '../../components/FormulaCard/FormulaCard';
-import { connect } from 'react-redux';
-import PropTypes from 'prop-types';
-import './FormulaContainer.css';
+import React, { Component } from "react";
+import { withRouter, NavLink } from "react-router-dom";
+import { FormulaCard } from "../../components/FormulaCard/FormulaCard";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+import "./FormulaContainer.css";
 
 export class FormulaContainer extends Component {
-
   componentDidMount() {
-    this.props.animateLogo('collapse-logo');
+    this.props.animateLogo("collapse-logo");
   }
 
   componentWillUnmount() {
-    this.props.animateLogo('expand-logo');
+    this.props.animateLogo("expand-logo");
   }
 
   filterResultCard = () => {
-    return this.props.filterResults.map((formula) => {
-        return (
-          <FormulaCard
-            key={formula.id}
-            {...formula}
-          />
-        );
+    return this.props.filterResults.map(formula => {
+      return <FormulaCard key={formula.id} {...formula} />;
     });
-  }
+  };
 
   FormulaCard = () => {
     return this.props.formulas.map(formula => {
-        return (
-          <FormulaCard
-            key={formula.id}
-            {...formula}
-          />
-        );
+      return <FormulaCard key={formula.id} {...formula} />;
     });
-  }
+  };
 
   render() {
     return (
-      <div className='form-container formula-container'>
-        <div className='link-container'>
-          <NavLink to='/'>back</NavLink>
-          <NavLink to='/search'>search</NavLink>
-          <NavLink to='/filter'>filter</NavLink>
+      <div className="form-container formula-container">
+        <div className="link-container">
+          <NavLink to="/">back</NavLink>
+          <NavLink to="/search">search</NavLink>
+          <NavLink to="/filter">filter</NavLink>
         </div>
-        <div className='formula-cards'>
-          {this.props.filterResults.length ? this.filterResultCard() : this.FormulaCard()}
+        <div className="formula-cards">
+          {this.props.filterResults.length
+            ? this.filterResultCard()
+            : this.FormulaCard()}
         </div>
       </div>
     );
   }
 }
 
-
-export const mapStateToProps = (state) => ({
+export const mapStateToProps = state => ({
   formulas: state.formulas,
   filterResults: state.filterResults
 });
