67f9d45b39ae8210253a9266d2b9ff1c891186f4
Jeremiah Stanley <jeremiah.j.stanley@gmail.com>
2020-03-24 10:41:15 -0600

    Lint File

--- src/reducers/filterReducer.js
+++ src/reducers/filterReducer.js
@@ -1,11 +1,11 @@
 export const filterReducer = (state = [], action) => {
-  switch(action.type) {
+  switch (action.type) {
     case "ADD_FILTER_RESULTS":
       return [...state, ...action.filterResults];
     case 'FILTER_RESULT_FETCH_DATA_SUCCESS':
       return action.filterResults;
     default: 
-    	return state;
+      return state;
   }
-}
+};
 
