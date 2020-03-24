204782efe838e3310def934dc70ca2910c22f90a
Jeremiah Stanley <jeremiah.j.stanley@gmail.com>
2020-03-24 10:46:35 -0600

    Lint file

--- src/containers/Form/Form.js
+++ src/containers/Form/Form.js
@@ -1,17 +1,17 @@
-import React, { Component } from 'react';
-import { NavLink, withRouter } from 'react-router-dom';
-import './Form.css'; 
+import React, { Component } from "react";
+import { NavLink, withRouter } from "react-router-dom";
+import "./Form.css";
 
 export class Form extends Component {
   render() {
     return (
       <div className="form-container links">
-				<NavLink to='/search'>search</NavLink>
-				<NavLink to='/filter'>filter</NavLink>
-				<NavLink to='/formulas'>browse</NavLink>
+        <NavLink to="/search">search</NavLink>
+        <NavLink to="/filter">filter</NavLink>
+        <NavLink to="/formulas">browse</NavLink>
       </div>
     );
   }
 }
 
-export default withRouter((Form));
+export default withRouter(Form);
