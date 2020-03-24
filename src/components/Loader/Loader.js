fb8e47cfb45086e18e74d2542013cfc081d3530a
Jeremiah Stanley <jeremiah.j.stanley@gmail.com>
2020-03-24 10:45:45 -0600

    Lint file

--- src/components/Loader/Loader.js
+++ src/components/Loader/Loader.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import { Dimmer, Loader, Segment } from 'semantic-ui-react';
+import React from "react";
+import { Dimmer, Loader, Segment } from "semantic-ui-react";
 
 export const Load = () => (
-    <Segment>
-      <Dimmer active inverted page>
-        <Loader size='large'>Loading</Loader>
-      </Dimmer>
-    </Segment>
+  <Segment>
+    <Dimmer active inverted page>
+      <Loader size="large">Loading</Loader>
+    </Dimmer>
+  </Segment>
 );
 
 export default Load;
