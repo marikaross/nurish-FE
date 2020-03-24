74802641b208b0965f8c2dcd4dbc576a854e4bdd
Jeremiah Stanley <jeremiah.j.stanley@gmail.com>
2020-03-24 10:47:31 -0600

    Lint file

--- src/containers/Search/Search.js
+++ src/containers/Search/Search.js
@@ -1,63 +1,63 @@
-import React, { Component } from 'react';
-import { fetchResults } from '../../thunks/fetchResults';
-import { withRouter, NavLink } from 'react-router-dom';
-import { addFilterResults } from '../../actions';
-import { Input} from 'semantic-ui-react';
-import { connect } from 'react-redux';
-import PropTypes from 'prop-types';
-import './Search.css';
+import React, { Component } from "react";
+import { fetchResults } from "../../thunks/fetchResults";
+import { withRouter, NavLink } from "react-router-dom";
+import { addFilterResults } from "../../actions";
+import { Input } from "semantic-ui-react";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+import "./Search.css";
 
 export class Search extends Component {
-	constructor() {
-		super();
+  constructor() {
+    super();
 
-		this.state = {
-			input: ''
-		};
-	}
+    this.state = {
+      input: ""
+    };
+  }
 
-	handleSubmit = async (event) => {
-		event.preventDefault();
-		let keyWord = this.state.input.replace(/\s/g, '');
-		let url = `https://nurish-app.herokuapp.com/api/v1/search?key_word=${keyWord}`;
-		await this.props.fetchResults(url);
-		this.props.history.push('/formulas');
-	}
+  handleSubmit = async event => {
+    event.preventDefault();
+    let keyWord = this.state.input.replace(/\s/g, "");
+    let url = `https://nurish-app.herokuapp.com/api/v1/search?key_word=${keyWord}`;
+    await this.props.fetchResults(url);
+    this.props.history.push("/formulas");
+  };
 
-	handleInput = (input) => {
-		this.setState({input});
-	}
+  handleInput = input => {
+    this.setState({ input });
+  };
 
-	render() {		
-		return (
-			<div className='form-container search-container'>
-				<div className='link-container'>
-					<NavLink to='/'>back</NavLink>
-					<NavLink to='/filter'>filter</NavLink>
-					<NavLink to='/formulas'>browse</NavLink>
-				</div>
-				<form className='search-field' onSubmit={this.handleSubmit}>
-        <Input 
-          action='Search'
-          onChange={(event) => this.handleInput(event.target.value)}
-          value={this.state.value}
-          placeholder='enter your criteria' 
-        />
-				</form>
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div className="form-container search-container">
+        <div className="link-container">
+          <NavLink to="/">back</NavLink>
+          <NavLink to="/filter">filter</NavLink>
+          <NavLink to="/formulas">browse</NavLink>
+        </div>
+        <form className="search-field" onSubmit={this.handleSubmit}>
+          <Input
+            action="Search"
+            onChange={event => this.handleInput(event.target.value)}
+            value={this.state.value}
+            placeholder="enter your criteria"
+          />
+        </form>
+      </div>
+    );
+  }
 }
 
-export const mapStateToProps = (state) => ({
-	filterResults: state.filterResults,
-	isLoading: state.isLoading,
+export const mapStateToProps = state => ({
+  filterResults: state.filterResults,
+  isLoading: state.isLoading,
   hasErrored: state.hasErrored
 });
 
-export const mapDispatchToProps = (dispatch) => ({
-	addFilterResults: (filterResults) => dispatch(addFilterResults(filterResults)),
-	fetchResults: (url) =>  dispatch(fetchResults(url))
+export const mapDispatchToProps = dispatch => ({
+  addFilterResults: filterResults => dispatch(addFilterResults(filterResults)),
+  fetchResults: url => dispatch(fetchResults(url))
 });
 
 Search.propTypes = {
